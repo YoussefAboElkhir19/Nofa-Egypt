f757c8d2754f922fbc568029f6ffd4c78b3bc8d4
SamyShaawat <samy.shaawat@gmail.com>
2024-09-23 22:00:50 +0300

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/nofa-egypt/", // Your repo name here
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
   },
-  base: "/nofa-egypt/", // Your repo name here
   server: {
     historyApiFallback: true, // Important for React Router to work
   },
