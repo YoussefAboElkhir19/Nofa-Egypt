d88e7b84814232e8ae9d7f8d412dc304ecc249ec
SamyShaawat <samy.shaawat@gmail.com>
2024-09-23 22:54:33 +0300

    fix deploy problem in base name

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -7,31 +7,37 @@ import Products from "../pages/Products";
 import EventsNewsletter from "../pages/EventsNewsletter";
 import Login from "../pages/Login";
 import ControlPanel from "../pages/ControlPanel";
-import VerifyEmail from "../pages/VerifyEmail"; // Import VerifyEmail
-import ProtectedRoute from "../components/Control Panel/ProtectedRoute.jsx"; // Importing the ProtectedRoute
+import VerifyEmail from "../pages/VerifyEmail";
+import ProtectedRoute from "../components/Control Panel/ProtectedRoute.jsx";
 
-const Router = createBrowserRouter([
+// Add the basename to ensure the app is served correctly from GitHub Pages
+const Router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "about-us", element: <About /> },
+        { path: "our-products", element: <Products /> },
+        { path: "events-newsletter", element: <EventsNewsletter /> },
+        { path: "contact-us", element: <Contact /> },
+        { path: "login", element: <Login /> },
+        {
+          path: "control-panel",
+          element: (
+            <ProtectedRoute>
+              <ControlPanel />
+            </ProtectedRoute>
+          ),
+        },
+        { path: "verify-email", element: <VerifyEmail /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/about-us", element: <About /> },
-      { path: "/our-products", element: <Products /> },
-      { path: "/events-newsletter", element: <EventsNewsletter /> },
-      { path: "/contact-us", element: <Contact /> },
-      { path: "/login", element: <Login /> },
-      {
-        path: "/control-panel",
-        element: (
-          <ProtectedRoute>
-            <ControlPanel />
-          </ProtectedRoute>
-        ),
-      },
-      { path: "/verify-email", element: <VerifyEmail /> }, // Add the VerifyEmail route here
-    ],
-  },
-]);
+    basename: "/nofa-egypt", // Important for GitHub Pages deployment
+  }
+);
 
 export default Router;
